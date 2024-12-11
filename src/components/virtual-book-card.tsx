6448e733364aaca44649c0119b31c3564ea492ee
mapokapo <leopetrovic11@gmail.com>
2024-12-11 14:37:29 +0100

    style: small style improvement on virtual book card

--- src/components/virtual-book-card.tsx
+++ src/components/virtual-book-card.tsx
@@ -76,14 +76,14 @@ const VirtualBookCard: React.FC<Props> = ({ virtualBook }) => {
             </div>
           </div>
           <CollapsibleContent>
-            <ul className="flex flex-col items-center gap-2">
-              <li className="flex items-center gap-1">
+            <ul className="flex w-full flex-col items-stretch divide-y">
+              <li className="flex items-center justify-between gap-1">
                 <span className="font-bold text-sum-red-foreground">ISBN:</span>
                 <span className="text-sum-red-foreground">
                   {virtualBook.isbn}
                 </span>
               </li>
-              <li className="flex items-center gap-1">
+              <li className="flex items-center justify-between gap-1">
                 <span className="font-bold text-sum-red-foreground">
                   Lokacija:
                 </span>
@@ -93,7 +93,7 @@ const VirtualBookCard: React.FC<Props> = ({ virtualBook }) => {
               </li>
               {virtualBook.digitalizedContent !== null && (
                 <>
-                  <li className="flex items-center gap-1">
+                  <li className="flex items-center justify-between gap-1">
                     <span className="font-bold text-sum-red-foreground">
                       Digitalizirao/la:
                     </span>
@@ -101,7 +101,7 @@ const VirtualBookCard: React.FC<Props> = ({ virtualBook }) => {
                       {virtualBook.digitalizedContent.digitalizedByUserName}
                     </span>
                   </li>
-                  <li className="flex items-center gap-1">
+                  <li className="flex items-center justify-between gap-1">
                     <span className="font-bold text-sum-red-foreground">
                       Datum digitaliziranja:
                     </span>
