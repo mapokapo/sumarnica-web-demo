6e252fe50ce4d1f0cffef0c5d8599250fbd162ce
mapokapo <leopetrovic11@gmail.com>
2024-12-11 11:07:48 +0100

    feat: add reserve button and reserve dialog to smart classroom items

---
+++ src/components/date-picker-button.tsx
@@ -0,0 +1,48 @@
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import React, { useState } from "react";
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  label?: string;
+};
+
+const DatePickerButton: React.FC<Props> = ({ className, label, ...rest }) => {
+  const [date, setDate] = useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+            className
+          )}
+          {...rest}>
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>{label ?? ""}</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DatePickerButton;
--- src/components/smart-class-room-card.tsx
+++ src/components/smart-class-room-card.tsx
@@ -1,3 +1,4 @@
+import DatePickerButton from "@/components/date-picker-button";
 import SmartClassRoomReservationItem from "@/components/smart-class-room-reservation-item";
 import { Button } from "@/components/ui/button";
 import {
@@ -5,6 +6,16 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { SmartClassRoom } from "@/lib/types/SmartClassRoom";
 import { cn } from "@/lib/utils";
 import { CircleCheck, CircleHelp, CircleX } from "lucide-react";
@@ -69,63 +80,93 @@ const SmartClassRoomCard: React.FC<Props> = ({ smartClassRoom }) => {
           )}
         </div>
       </div>
-      {smartClassRoom.reservations.length === 0 ? (
-        <p className="text-center text-sum-blue-muted-foreground">
-          Ova pametna učionica nema nijednu rezervaciju.
-        </p>
-      ) : (
-        <div className="flex flex-col gap-2">
-          {smartClassRoom.reservations.some(
-            r =>
-              r.reservationFrom.getTime() < Date.now() &&
-              r.reservationTo.getTime() > Date.now()
-          ) ? (
-            <div className="flex flex-col rounded-lg border-2 border-sum-red">
-              <span className="bg-sum-red p-2 text-sm text-sum-red-foreground">
-                Trenutni događaj
-              </span>
-              <div className="p-2">
-                <SmartClassRoomReservationItem
-                  className="animate-pulse"
-                  reservation={
-                    smartClassRoom.reservations.find(
-                      r =>
-                        r.reservationFrom.getTime() < Date.now() &&
-                        r.reservationTo.getTime() > Date.now()
-                    ) as unknown as SmartClassRoom["reservations"][0]
-                  }
-                />
+      <Dialog>
+        {smartClassRoom.reservations.length === 0 ? (
+          <div className="flex flex-col gap-2">
+            <p className="text-center text-sum-blue-muted-foreground">
+              Ova pametna učionica nema nijednu rezervaciju.
+            </p>
+            <DialogTrigger asChild>
+              <Button variant="sum">Rezerviraj</Button>
+            </DialogTrigger>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {smartClassRoom.reservations.some(
+              r =>
+                r.reservationFrom.getTime() < Date.now() &&
+                r.reservationTo.getTime() > Date.now()
+            ) ? (
+              <div className="flex flex-col rounded-lg border-2 border-sum-red">
+                <span className="bg-sum-red p-2 text-sm text-sum-red-foreground">
+                  Trenutni događaj
+                </span>
+                <div className="p-2">
+                  <SmartClassRoomReservationItem
+                    className="animate-pulse"
+                    reservation={
+                      smartClassRoom.reservations.find(
+                        r =>
+                          r.reservationFrom.getTime() < Date.now() &&
+                          r.reservationTo.getTime() > Date.now()
+                      ) as unknown as SmartClassRoom["reservations"][0]
+                    }
+                  />
+                </div>
               </div>
-            </div>
-          ) : occupationStatus === "reserved" ? (
-            <span className="mb-4 text-center font-semibold text-yellow-400">
-              Ova pametna učionica je rezervirana za budući datum.
-            </span>
-          ) : null}
-          <Collapsible className="flex flex-col gap-2">
-            <CollapsibleTrigger asChild>
-              <Button
-                variant="sum-secondary"
-                size="sm"
-                className="w-full">
-                Prikaži sve rezervacije
-              </Button>
-            </CollapsibleTrigger>
-            <CollapsibleContent>
-              <ol className="flex flex-col gap-2">
-                {smartClassRoom.reservations.map(reservation => (
-                  <li key={reservation.id}>
-                    <SmartClassRoomReservationItem
-                      key={reservation.id}
-                      reservation={reservation}
-                    />
-                  </li>
-                ))}
-              </ol>
-            </CollapsibleContent>
-          </Collapsible>
-        </div>
-      )}
+            ) : occupationStatus === "reserved" ? (
+              <span className="mb-4 text-center font-semibold text-yellow-400">
+                Ova pametna učionica je rezervirana za budući datum.
+              </span>
+            ) : null}
+            <DialogTrigger asChild>
+              <Button variant="sum">Rezerviraj</Button>
+            </DialogTrigger>
+            <Collapsible className="flex flex-col gap-2">
+              <CollapsibleTrigger asChild>
+                <Button
+                  variant="sum-secondary"
+                  size="sm"
+                  className="w-full">
+                  Prikaži sve rezervacije
+                </Button>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <ol className="flex flex-col gap-2">
+                  {smartClassRoom.reservations.map(reservation => (
+                    <li key={reservation.id}>
+                      <SmartClassRoomReservationItem
+                        key={reservation.id}
+                        reservation={reservation}
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </CollapsibleContent>
+            </Collapsible>
+          </div>
+        )}
+        <DialogContent className="border-none bg-sum-blue text-sum-blue-foreground">
+          <DialogHeader>
+            <DialogTitle>Rezervacija pametne učionice</DialogTitle>
+            <DialogDescription className="text-sum-blue-muted-foreground">
+              Rezervirajte ovu pametnu učionicu za svoje predavanje ili
+              radionicu.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col">
+            <DatePickerButton
+              className="w-full"
+              label="Izaberi datum"
+            />
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="sum-secondary">Zatvori</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
