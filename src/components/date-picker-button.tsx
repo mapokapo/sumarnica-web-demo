b64f957e391536fb684cdd3d5697cc73c971c46a
mapokapo <leopetrovic11@gmail.com>
2024-12-11 14:38:08 +0100

    deps: add shadcn components (date picker, label, sonner)

--- src/components/date-picker-button.tsx
+++ src/components/date-picker-button.tsx
@@ -10,13 +10,29 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { PopoverClose } from "@radix-ui/react-popover";
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   label?: string;
+  dateValue?: Date;
+  onPickDate?: (date: Date) => void;
 };
 
-const DatePickerButton: React.FC<Props> = ({ className, label, ...rest }) => {
-  const [date, setDate] = useState<Date>();
+const DatePickerButton: React.FC<Props> = ({
+  className,
+  label,
+  dateValue,
+  onPickDate,
+  ...rest
+}) => {
+  const [date, setDate] = useState<Date | null>(dateValue ?? null);
+
+  const handleSelectDate = (date: Date | null) => {
+    if (onPickDate !== undefined && date !== null) {
+      onPickDate(date);
+    }
+  };
 
   return (
     <Popover>
@@ -30,16 +46,45 @@ const DatePickerButton: React.FC<Props> = ({ className, label, ...rest }) => {
           )}
           {...rest}>
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>{label ?? ""}</span>}
+          {date ? format(date, "dd.MM.yyyy HH:mm") : <span>{label ?? ""}</span>}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent className="flex w-auto flex-col gap-2 p-0">
         <Calendar
           mode="single"
-          selected={date}
-          onSelect={setDate}
+          selected={date ?? undefined}
+          onSelect={d =>
+            setDate(
+              d !== undefined
+                ? new Date(
+                    `${format(d, "yyyy-MM-dd")}T${format(date ?? new Date(), "HH:mm")}`
+                  )
+                : null
+            )
+          }
           initialFocus
         />
+        <Input
+          className="mx-auto w-[90%]"
+          type="time"
+          value={format(date ?? new Date(), "HH:mm")}
+          onChange={e =>
+            setDate(
+              new Date(
+                `${format(date ?? new Date(), "yyyy-MM-dd")}T${e.target.value}`
+              )
+            )
+          }
+        />
+        <PopoverClose asChild>
+          <Button
+            className="mx-auto mb-4 w-[90%]"
+            onClick={() => {
+              handleSelectDate(date);
+            }}>
+            {date ? "Odaberi" : "Zatvori"}
+          </Button>
+        </PopoverClose>
       </PopoverContent>
     </Popover>
   );
